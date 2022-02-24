98327109bf27ae6489002fca7741427bcf8276d3
“linjinsheng” <linjinshengscut@126.com>
2022-02-24 09:01:12 +0800

    修改

--- 83、React中的setState同步还是异步/demo/src/App.js
+++ 83、React中的setState同步还是异步/demo/src/App.js
@@ -9,21 +9,21 @@ export default class App extends Component {
   };
 
   onClick = ()=>{
-    //  异步
-    console.log('更新前, num---',this.state.num);
-    this.setState({
-      num: this.state.num + 1
-    })
-    console.log('更新后, num---',this.state.num);
+    // //  异步
+    // console.log('更新前, num---',this.state.num);
+    // this.setState({
+    //   num: this.state.num + 1
+    // })
+    // console.log('更新后, num---',this.state.num);
 
-    // //  同步
-    // console.log('setTimeout----更新前, num---',this.state.num);
-    // setTimeout(()=>{
-    //   this.setState({
-    //     num: this.state.num + 1
-    //   })
-    //   console.log('setTimeout----更新后, num---',this.state.num);
-    // }, 0)
+    //  同步
+    console.log('setTimeout----更新前, num---',this.state.num);
+    setTimeout(()=>{
+      this.setState({
+        num: this.state.num + 1
+      })
+      console.log('setTimeout----更新后, num---',this.state.num);
+    }, 0)
   }
   
   render() {
